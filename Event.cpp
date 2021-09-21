6e212205e9a6434a2d081767248738cee3931e8d
Niccolo10 <parlanti.niccolo@gmail.com>
2021-09-21 15:16:44 +0200

    Implementazione della classe event

--- Event.cpp
+++ Event.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Event.h"
+#include <iostream>
+
+Event::Event(std::string description, std::string title, Date date) : eventDate(date) {
+    this->eventDescription = description;
+    this->eventTitle = title;
+}
+
+const std::string &Event::getEventDescription() const {
+    return eventDescription;
+}
+
+void Event::setEventDescription(const std::string &eventDescription) {
+    Event::eventDescription = eventDescription;
+}
+
+const std::string &Event::getEventTitle() const {
+    return eventTitle;
+}
+
+void Event::setEventTitle(const std::string &eventTitle) {
+    Event::eventTitle = eventTitle;
+}
+
+const Date &Event::getEventDate() const {
+    return eventDate;
+}
+
+void Event::setEventDate(const Date &eventDate) {
+    Event::eventDate = eventDate;
+}
+
+void Event::display() const {
+    std::cout<<"Title:        " << eventTitle << std::endl;
+    std::cout<<"Description:  " << eventDescription << std::endl;
+    std::cout<<"Date:         " << eventDate.getDay() << "/" << eventDate.getMonth() << "/" << eventDate.getYear() << std::endl;
+}
--- Event.h
+++ Event.h
@@ -6,10 +6,31 @@
 #define TODOLIST_PROJECT_EVENT_H
 
 
+#include <string>
+#include "Date.h"
+
 class Event {
 public:
+    Event (std::string description, std::string title, Date date);
+
+    const std::string &getEventDescription() const;
+
+    void setEventDescription(const std::string &eventDescription);
+
+    const std::string &getEventTitle() const;
+
+    void setEventTitle(const std::string &eventTitle);
+
+    const Date &getEventDate() const;
+
+    void setEventDate(const Date &eventDate);
+
+    void display()const;
 
 private:
+    std::string eventDescription;
+    std::string eventTitle;
+    Date eventDate;
 
 };
 
